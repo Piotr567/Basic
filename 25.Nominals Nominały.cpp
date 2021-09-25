9450ec0e4d447f6a16f66668c3cb3eed9354c4a2
Piotr567 <61947900+Piotr567@users.noreply.github.com>
2021-09-25 11:58:02 +0200

    Add files via upload

---
+++ 25.Nominals Nominały.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+void nominals(int nominal)
+{
+    if(nominal > 0)
+    {
+        int kwota = 1;
+        while (kwota < nominal)
+        {
+
+            for(int index = 0; index < nominal; index ++)
+            {
+                if (index == 1 || index == 2 || index == 5)
+                {
+                    std::cout << index * kwota << " ";
+                }
+            }
+            kwota = kwota * 10;
+        }
+    }
+}
+
+
+
+
+int main()
+{
+    int nominal;
+    std::cin >> nominal;
+    nominals(nominal);
+}
